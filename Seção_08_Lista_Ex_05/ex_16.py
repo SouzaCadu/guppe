524a09a1419f827d03963b10f9a54a7ecd574065
Carlos Souza <cadu.souza81@gmail.com>
2020-12-30 19:57:58 -0300

    Initial Commit

--- Seção_08_Lista_Ex_05/ex_16.py
+++ Seção_08_Lista_Ex_05/ex_16.py
@@ -1,3 +1,17 @@
 """
-
+Faça uma função chamada DesenhaLinha. Ele deve desenhar uma linha na tela usando
+vários símbolos de igual (Ex.: ========). A função recebe por parâmetro quantos
+sinais de igual serão mostrados.
 """
+
+
+def desenha_linha(tamanho):
+    """
+    Desenha uma linha na tela usando vários símbolos de igual
+    :param tamanho: Quantos sinais de igual serão mostrados
+    :return: vários símbolos de igual (Ex.: ========)
+    """
+    print(f"{'=' * tamanho}")
+
+
+print(desenha_linha(12))
