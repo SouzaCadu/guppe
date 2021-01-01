5c2f7f076df67b8c2f5e0c4523d66e8d59e13d12
Carlos Souza <cadu.souza81@gmail.com>
2021-01-01 17:42:09 -0300

    Initial Commit

--- Seção_08_Lista_Ex_05/ex_34.py
+++ Seção_08_Lista_Ex_05/ex_34.py
@@ -1,3 +1,22 @@
 """
-
+faça uma função não recursiva que receba um número inteiro
+positivo ímpar N e retorne o fatorial duplo desse número
 """
+
+
+def fatorial_duplo(num):
+    """
+    Função não recursiva que receba um número inteiro
+    positivo ímpar N e retorna o fatorial duplo desse número
+    :param num:  Qualquer número inteiro positivo ímpar
+    :return: O fatorial duplo desse número
+    """
+    if num % 2 != 0 and num > 0:
+        produto = 1
+        for i in range(1, num + 1, 2):
+            produto *= i
+        return produto
+    return "Digite um número inteiro ímpar positivo!"
+
+
+print(fatorial_duplo(75))
--- Seção_08_Lista_Ex_05/ex_35.py
+++ Seção_08_Lista_Ex_05/ex_35.py
@@ -1,3 +1,22 @@
 """
-
+faça uma função não recursiva que receba u número inteiro positivo
+N e retorne o fatorial quádruplo desse número
 """
+
+
+from math import factorial
+
+
+def fatorial_quadruplo(num):
+    """
+    Função não recursiva que recebe um número inteiro positivo
+    N e retorne o fatorial quádruplo desse número
+    :param num: Qualquer número inteiro positivo
+    :return: O fatorial quádruplo desse número
+    """
+    if num >= 0:
+        return factorial(2 * num) / factorial(num)
+    return "Digite um número inteiro positivo."
+
+
+print(fatorial_quadruplo(9))
