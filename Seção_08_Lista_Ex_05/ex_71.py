9ad9b02ba1b8717b4317153909e085be9854012a
Carlos Souza <cadu.souza81@gmail.com>
2021-01-05 22:44:36 -0300

    Initial Commit

--- Seção_08_Lista_Ex_05/ex_71.py
+++ Seção_08_Lista_Ex_05/ex_71.py
@@ -1,3 +1,36 @@
 """
-
+para representar um ponto em uma grade 2D, implemente uma função que indique se um
+ponto p está localizado dentro ou fora de um retângulo.
+o retângulo é definido por seus vértices inferior esquerdo v1 e superior direito v2.
+a função deve retornar 1 caso o ponto esteja localizado dentro do retângulo e 0 caso contrário.
 """
+
+
+def dentro_ret(x1, y1, x2, y2, p1, p2):
+    """
+    Função que recebe as posições do vértice inferior esquerdo e superior direto de um
+    retângulo
+    :param x1: Posição da coordenada X do vértice inferior esquerdo
+    :param y1: Posição da coordenada Y do vértice inferior esquerdo
+    :param x2: Posição da coordenada X do vértice superior direito
+    :param y2: Posição da coordenada Y do vértice superior direito
+    :param p1: Posição da coordenada X do ponto indicado
+    :param p2: Posição da coordenada Y do ponto indicado
+    :return: Retorna 1 caso as coordenadas informadas esteja dentro do retângulo e 0 caso contrário.
+    """
+
+    if x1 <= p1 <= x2:
+        if y1 <= p2 <= y2:
+            return 1
+    return 0
+
+
+vertice_x1 = int(input("Local da coordenada x do vértice inferior esquerdo do retângulo: "))
+vertice_y1 = int(input("Local da coordenada y do vértice inferior esquerdo do retângulo: "))
+vertice_x2 = int(input("Local da coordenada x do vértice superior direita do retângulo: "))
+vertice_y2 = int(input("Local da coordenada y do vértice superior direita do retângulo: "))
+
+coordenada_p1 = int(input("Local da coordenada x do ponto: "))
+coordenada_p2 = int(input("Local da coordenada y do ponto: "))
+
+print(f"\n{dentro_ret(vertice_x1, vertice_y1, vertice_x2, vertice_y2, coordenada_p1, coordenada_p2)}")
--- Seção_08_Lista_Ex_05/ex_72.py
+++ Seção_08_Lista_Ex_05/ex_72.py
@@ -1,3 +1,29 @@
 """
-
+para representar um vetor de R³, implemente uma função que calcule a soma
+de dois vetores. Essa função deve obedecer ao protótipo:
+void soma (struct Vetor* v1, struct Vetor* v2, struct Vetor* res);
+onde os parâmetros v1 e v2 são ponteiros para os vetores a serem somados, e o parâmetro
+res é um ponteiro para uma estrutura vetor onde o resultado da operação deve ser
+armazenado
 """
+
+
+def soma(v1, v2, mult):
+    """
+    Função que recebe dois vetores e retorna outro vetor, com a soma dos elementos dos vetores
+    recebidos por parâmetro
+    :param v1: Recebe um vetor de números
+    :param v2: Recebe um vetor de números
+    :param mult: Vetor resultado da multiplicação v1 v2
+    :return: Retorna um terceiro vetor com a soma dos elementos de cada vetor recebido. Caso
+    os valores informados não sejam números, retorna um valor do tipo None
+    """
+    for i, z in zip(v1, v2):
+        mult.append(i * z)
+    return mult
+
+
+vet1 = [2, 3, 5]
+vet2 = [-3, 8, 9]
+res = []
+print(soma(vet1, vet2, res))
