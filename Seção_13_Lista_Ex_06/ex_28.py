58fa95654852d1eb27b8ad1378244f54462304f8
Carlos Souza <cadu.souza81@gmail.com>
2021-02-05 22:20:05 -0300

    Initial Commit

--- Seção_13_Lista_Ex_06/ex_28.py
+++ Seção_13_Lista_Ex_06/ex_28.py
@@ -13,3 +13,32 @@
     Licáf otium átse avorp A
     PA ed avortp ed aid é ejoH
 """
+
+
+import os
+
+print("Informe o caminho completo de 2 arquivos. O conteúdo inserido no  primeiro arquivo será limitado"
+      "em 30 caracteres por linha. O segundo será criado com o mesmo conteúdo do primeiro em ordem inversa.")
+
+path1 = str(input("Informe o caminho do primeiro arquivo: "))
+
+while True:
+    path2 = str(input("Informe o caminho do segundo arquivo: "))
+    if os.path.isfile(path2):
+        print("O arquivo já existe!")
+    else:
+        break
+
+try:
+    with open(path1, "w") as arquivo:
+        texto = str(input("\nInsira o texto do arquivo de entrada: ")).strip()
+        conteudo = ""
+        for palavra in range(len(texto)):
+            conteudo += f"{texto[palavra]}\n" if (palavra + 1) % 30 == 0 else f"{texto[palavra]}"
+        arquivo.write(conteudo)
+    with open(path1, "r") as leitura:
+        with open(path2, "w") as novo_arquivo:
+            novo_arquivo.write(leitura.read()[::-1])
+    print(f"\nTexto alterado com sucesso em {path2}!")
+except FileNotFoundError:
+    print("\nArquivo informado não encontrado!")
